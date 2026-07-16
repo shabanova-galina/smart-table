6b32b110c756824033f5d3275899f7f9995f5946
Galina Shabanova <shabanova.galina2011@yandex.ru>
2026-07-16 19:31:25 +0300

    first

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,41 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
 
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {
+                            const option = document.createElement('option'); // создаем <option>
+                            option.value = name; // задаем значение
+                            option.textContent = name; // задаем текст
+                            return option; // возвращаем созданный элемент                        
+                            })
+
+        )
+     })
+
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const fieldName = action.dataset.field;
+        // Очищаем текстовые поля
+            document.querySelectorAll('.filter-row input[type="text"]').forEach(input => {
+            input.value = '';
+            if (state) {
+                state[fieldName] = '';
+            };
+        })
+    }
+
+            
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
